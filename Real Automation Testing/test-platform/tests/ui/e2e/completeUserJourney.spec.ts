7e4b89578484000e91c4481d0d07f28484ce64f8
gauravkhope <gauravkhope31@gmail.com>
2026-05-23 23:46:13 +0530

    added complete user journey part 2

--- Real Automation Testing/test-platform/tests/ui/e2e/completeUserJourney.spec.ts
+++ Real Automation Testing/test-platform/tests/ui/e2e/completeUserJourney.spec.ts
@@ -2,10 +2,19 @@ import { test } from '../../../core/fixtures/base.fixture';
 import users from '../../../data/static/users.json';
 import { LoginAssertions } from '../../../validation/ui/auth/loginAssertions';
 
- test('Valid login', async ({ loginFlow, loginPage }) => {
+ test('complete user journey', async ({ loginFlow, loginPage , page }) => {
     const user = users.validUser;
 
     await loginFlow.login(user.email, user.password);
 
     await LoginAssertions.expectLoginSuccess(loginPage);
-  });
+ const productName = "Galaxy S25 ultra";
+
+ const productCard =  page.getByTestId("trending-product-card").filter({
+        has: page.getByRole("heading", { name: productName , exact: true}),
+  })
+  await productCard.click();
+  await page.getByRole("button", { name: "Add to Wishlist" }).click();
+  await page.getByTestId("navbar-wishlist").click();
+  await page.waitForTimeout(2000);
+})
